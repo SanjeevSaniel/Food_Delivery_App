3f22f9511eab11531c118045fa986202470e7a1d
Sanjeev Kujur <sanjeevsaniel@outlook.in>
2023-03-20 03:04:45 +0530

    Newer Builds

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
-import axios from "axios";
+// import axios from "axios";
 
 import NavbarComponent from "./common/NavbarContainer";
 
@@ -16,35 +16,31 @@ export const CartContext = createContext();
 export const CartCountContext = createContext();
 
 function App() {
-  // const restaurantsData = require("./api/Restaurants.json");
-  // const restaurantsData = require("http://localhost:5000/api/restaurants");
+  const restaurants = require("./api/Restaurants.json");
 
-  // const [restaurants, setRestaurants] = useState(
-  //   "http://localhost:5000/api/restaurants"
-  // );
-  const URL = "http://localhost:5000";
-  const [restaurants, setRestaurants] = useState([]);
+  // const URL = "http://localhost:5000";
+  // const [restaurants, setRestaurants] = useState([]);
   const [cart, setCart] = useState([]);
-
-  const fetchRestaurants = async () => {
-    const data = await axios
-      .get(`${URL}/api/restaurants`)
-      .then((response) => response.data);
-    setRestaurants(data);
-  };
 
   const handleCart = (value) => {
     setCart(value);
   };
 
-  useEffect(() => {
-    fetchRestaurants();
-  }, []);
+  // const fetchRestaurants = async () => {
+  //   const data = await axios
+  //     .get(`${URL}/api/restaurants`)
+  //     .then((response) => response.data);
+  //   setRestaurants(data);
+  // };
 
   useEffect(() => {
     let result = JSON.parse(localStorage.getItem("food_cart"));
     if (result !== null) setCart(result);
   }, []);
+
+  // useEffect(() => {
+  //   fetchRestaurants();
+  // }, []);
 
   return (
     <div className="App">
