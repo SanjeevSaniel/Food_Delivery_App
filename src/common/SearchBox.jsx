7d15459874ba49d1da381d1fc856b559d26d1c10
Sanjeev Kujur <sanjeevsaniel@outlook.in>
2023-03-19 14:51:21 +0530

    Newer Builds

--- src/common/SearchBox.jsx
+++ src/common/SearchBox.jsx
@@ -1,11 +1,14 @@
 import React from "react";
-import Button from "react-bootstrap/Button";
+// import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
 
 const SearchBox = ({ value, onChange }) => {
   return (
     <InputGroup className="mb-3 searchbox">
+      <span className="input-group-text">
+        <span class="material-symbols-outlined">search</span>
+      </span>
       <Form.Control
         placeholder="Find your food"
         aria-label="Find your food"
@@ -13,9 +16,10 @@ const SearchBox = ({ value, onChange }) => {
         value={value}
         onChange={(e) => onChange(e.currentTarget.value)}
       />
-      <Button variant="outline-secondary" id="button-addon">
+
+      {/* <Button disabled variant="outline-secondary" id="button-addon">
         Find Food
-      </Button>
+      </Button> */}
     </InputGroup>
   );
 };
